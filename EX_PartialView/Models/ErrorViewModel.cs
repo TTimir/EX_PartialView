00c2fb2da2d8d6dce7fffb87c069d72289e91bee
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-10-22 21:19:22 +0530

    Add project files.

---
+++ EX_PartialView/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EX_PartialView.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
